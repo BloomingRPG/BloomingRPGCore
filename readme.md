1aeabf5ecaaa2a8c3e27fe04bcc1742e368aab4c
Mr-IK <178kazu.ironman@gmail.com>
2019-06-14 01:30:28 +0900

    v0.1.0 readme.md update

--- readme.md
+++ readme.md
@@ -6,6 +6,8 @@
 ### Todo
 次回の大型アップデート: v0.1.1  
 https://github.com/bloomingrpg/BloomingRPGCore/issues/1  
+次々回の大型アップデート: v0.1.2  
+https://github.com/bloomingrpg/BloomingRPGCore/issues/2  
 
 ### Version History
 v0.1.0: 作成。 readme.md作成&編集。
--- src/main/java/jp/mkserver/bloom/bloomingrpgcore/BloomingRPGCore.java
+++ src/main/java/jp/mkserver/bloom/bloomingrpgcore/BloomingRPGCore.java
@@ -4,6 +4,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class BloomingRPGCore extends JavaPlugin {
 
+    String prefix = "§e§l[§c§lB§6§lRPG§e§l]§§r";
+
     @Override
     public void onEnable() {
         // Plugin startup logic
