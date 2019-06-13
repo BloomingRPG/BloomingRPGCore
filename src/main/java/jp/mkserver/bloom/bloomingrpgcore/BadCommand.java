11f5c59d655f8fb85f9c7b6d6e5a41af2cfa7b35
Mr-IK <178kazu.ironman@gmail.com>
2019-06-14 08:53:18 +0900

    v0.1.1-dev added BadCommand.java

---
+++ src/main/java/jp/mkserver/bloom/bloomingrpgcore/BadCommand.java
@@ -0,0 +1,62 @@
+package jp.mkserver.bloom.bloomingrpgcore;
+
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.server.TabCompleteEvent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BadCommand implements Listener {
+
+    List<String> badCommandList = new ArrayList<>();
+
+    private BloomingRPGCore plugin;
+
+    public BadCommand(BloomingRPGCore plugin){
+        this.plugin = plugin;
+        initBadCommand();
+        plugin.getServer().getPluginManager().registerEvents(this,plugin);
+    }
+
+    //このメソッドで禁止コマンドを指定する
+    //指定されたコマンドはタブ保管も不可能になる
+    private void initBadCommand(){
+        badCommandList.add("/pl");
+        badCommandList.add("/plugins");
+        badCommandList.add("/bukkit:pl");
+        badCommandList.add("/bukkit:plugins");
+        badCommandList.add("/?");
+        badCommandList.add("/bukkit:?");
+        badCommandList.add("/help");
+        badCommandList.add("/bukkit:help");
+        badCommandList.add("/ver");
+        badCommandList.add("/version");
+        badCommandList.add("/bukkit:version");
+        badCommandList.add("/bukkit:ver");
+    }
+
+
+    @EventHandler
+    public void onCommandExecute(PlayerCommandPreprocessEvent e){
+        if(badCommandList.contains(e.getMessage())){
+            e.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onTabComplete(TabCompleteEvent event) {
+        if (!event.getBuffer().startsWith("/")) {
+            return; // not a command, or console entered command. ignore.
+        }
+        CommandSender sender = event.getSender();
+        if (sender.hasPermission("brpg.core.tabbypass")){
+            return; // has bypass. ignore
+        }
+        List<String> comple = event.getCompletions();
+        comple.removeAll(badCommandList);
+        event.setCompletions(comple);
+    }
+}
--- src/main/java/jp/mkserver/bloom/bloomingrpgcore/BloomingRPGCore.java
+++ src/main/java/jp/mkserver/bloom/bloomingrpgcore/BloomingRPGCore.java
@@ -6,10 +6,12 @@ public final class BloomingRPGCore extends JavaPlugin {
 
     String prefix = "§e§l[§c§lB§6§lRPG§e§l]§§r";
 
+    private BadCommand badCommand;
+
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        badCommand = new BadCommand(this);
     }
 
     @Override
