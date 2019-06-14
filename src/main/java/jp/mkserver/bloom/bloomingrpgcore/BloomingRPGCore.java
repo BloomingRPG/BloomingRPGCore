939e9fe44a345e52b048a66f5ef2a95021564dbf
Mr-IK <178kazu.ironman@gmail.com>
2019-06-14 09:51:12 +0900

    v0.1.1-dev added LoginBonus.java

--- src/main/java/jp/mkserver/bloom/bloomingrpgcore/BloomingRPGCore.java
+++ src/main/java/jp/mkserver/bloom/bloomingrpgcore/BloomingRPGCore.java
@@ -1,21 +1,84 @@
 package jp.mkserver.bloom.bloomingrpgcore;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Arrays;
+import java.util.UUID;
 
 public final class BloomingRPGCore extends JavaPlugin {
 
     String prefix = "§e§l[§c§lB§6§lRPG§e§l]§§r";
 
+    MySQLManagerV2 mysql;
     private BadCommand badCommand;
+    private LoginBonus loginBonus;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        mysql = new MySQLManagerV2(this,"BRPG-Core");
         badCommand = new BadCommand(this);
+        loginBonus = new LoginBonus(this);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
     }
+
+
+
+    public ItemStack createUnbitem(String name, String[] lore, Material item, int dura, boolean pikapika){
+        ItemStack items = new ItemStack(item,1,(short)dura);
+        ItemMeta meta = items.getItemMeta();
+        meta.setLore(Arrays.asList(lore));
+        meta.setDisplayName(name);
+        meta.addItemFlags(ItemFlag.HIDE_UNBREAKABLE);
+        meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+        if(pikapika){
+            meta.addEnchant(Enchantment.ARROW_FIRE,1,true);
+            meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+        }
+        meta.setUnbreakable(true);
+        items.setItemMeta(meta);
+        return items;
+    }
+
+    public ItemStack createItem(String name, String[] lore, Material item, int dura, boolean pikapika){
+        ItemStack items = new ItemStack(item,1,(short)dura);
+        ItemMeta meta = items.getItemMeta();
+        meta.setLore(Arrays.asList(lore));
+        meta.setDisplayName(name);
+        if(pikapika){
+            meta.addEnchant(Enchantment.ARROW_FIRE,1,true);
+            meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+        }
+        items.setItemMeta(meta);
+        return items;
+    }
+
+    public ItemStack createSkullitem(String name, String[] lore, UUID playeruuid, boolean pikapika){
+        ItemStack items = new ItemStack(Material.SKULL_ITEM,1,(short)3);
+        SkullMeta meta = (SkullMeta) items.getItemMeta();
+        meta.setLore(Arrays.asList(lore));
+        meta.setDisplayName(name);
+        meta.addItemFlags(ItemFlag.HIDE_UNBREAKABLE);
+        meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+        if(pikapika){
+            meta.addEnchant(Enchantment.ARROW_FIRE,1,true);
+            meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+        }
+        meta.setUnbreakable(true);
+        meta.setOwningPlayer(Bukkit.getPlayer(playeruuid));
+        items.setItemMeta(meta);
+        return items;
+    }
+
 }
---
+++ src/main/java/jp/mkserver/bloom/bloomingrpgcore/LoginBonus.java
@@ -0,0 +1,145 @@
+package jp.mkserver.bloom.bloomingrpgcore;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
+
+public class LoginBonus implements Listener {
+
+    private BloomingRPGCore plugin;
+    private static final String loginMessage = "§e<player> さんがBloomRPGに参加しました"; //通常ログインメッセージ。<player>はプレイヤー名に置き換え
+    private static final String firstloginMessage = "§d<player> さんがBloomRPGの世界に降り立ちました"; //初回ログインメッセージ。<player>はプレイヤー名に置き換え
+
+    public LoginBonus(BloomingRPGCore plugin){
+        this.plugin = plugin;
+        plugin.getServer().getPluginManager().registerEvents(this,plugin);
+    }
+
+    //そのログインは 前回から24時間以上経過している/初めて入った/前回から24時間経過していない
+    enum LoginType{
+        getbonuslogin,firstlogin,defaultlogin
+    }
+
+    //そのログインタイプは？
+    private LoginType checkLoginType(Player p){
+        if(!checkPlayerLogContain(p)){
+            return LoginType.firstlogin;
+        }
+        MySQLManagerV2.Query query = plugin.mysql.query("SELECT * from login_log WHERE uuid = '" + p.getUniqueId().toString() + "';");
+        ResultSet rs = query.getRs();
+        if(rs==null){
+            query.close();
+            return LoginType.firstlogin;
+        }
+        try {
+            if(rs.next()){
+                Date date = rs.getDate("bonus_date");
+                long dlo = date.getTime();
+                long nowlo = new Date().getTime();
+                long dayDiff = ( nowlo - dlo  ) / (1000 * 60 * 60 * 24 );
+                if(dayDiff>=1){
+                    query.close();
+                    return LoginType.getbonuslogin;
+                }else{
+                    query.close();
+                    return LoginType.defaultlogin;
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        query.close();
+        return LoginType.firstlogin;
+    }
+
+    //そのプレイヤーのデータがログインログに存在する？
+    private boolean checkPlayerLogContain(Player p){
+        MySQLManagerV2.Query query = plugin.mysql.query("SELECT * from login_log WHERE uuid = '" + p.getUniqueId().toString() + "';");
+        ResultSet rs = query.getRs();
+        if(rs==null){
+            query.close();
+            return false;
+        }
+        try {
+            if(rs.next()){
+                query.close();
+                return true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        query.close();
+        return false;
+    }
+
+    //DataBaseの情報をアップデート(作成)！
+    private void updatePlayerLog(Player p,boolean isbonus){
+        if(!checkPlayerLogContain(p)){
+            createPlayerLog(p);
+            return;
+        }
+        if(isbonus){
+            plugin.mysql.execute("UPDATE login_log SET bonus_date = CURRENT_TIMESTAMP WHERE uuid = '"+p.getUniqueId().toString()+"';");
+        }else{
+            plugin.mysql.execute("UPDATE login_log WHERE uuid = '"+p.getUniqueId().toString()+"';");
+        }
+    }
+
+    //DataBaseに情報をinsert(作成)！
+    private void createPlayerLog(Player p){
+        plugin.mysql.execute("INSERT INTO login_log (player,uuid)  VALUES ('"+p.getName()+"','"+p.getUniqueId().toString()+"');");
+    }
+
+    //ログインボーナスで実行されるメソッド
+    private void giveLoginBonus(Player p){
+        p.sendMessage("§e本日もログインいただきありがとうございます❤");
+        ItemStack apple = plugin.createUnbitem("§cログインアップル",
+                        new String[]{"§eログインボーナスとしてもらえる美味しいリンゴ。",
+                                "§e裏に油性ペンで廃棄と書いてある。"},
+                        Material.APPLE,0,false);
+        apple.setAmount(8);
+        p.getInventory().addItem(apple);
+    }
+
+    //初回ログインボーナスで実行されるメソッド
+    private void firstLoginBonus(Player p){
+        p.sendMessage("§d初めてログインいただきありがとうございます❤");
+        ItemStack apple = plugin.createUnbitem("§cログインアップル",
+                new String[]{"§d初ログインボーナスとしてもらえる美味しいリンゴ。",
+                        "§e裏に水性ペンで初心者用と書いてある。"},
+                Material.APPLE,0,false);
+        apple.setAmount(16);
+        p.getInventory().addItem(apple);
+        ItemStack sword = plugin.createItem("§7石の剣",
+                new String[]{"§eなぜか最初から持っている§8ザコい§e剣。",
+                        "§eこれを使うぐらいなら木の枝の方がマシだ。"},
+                Material.STONE_SWORD,0,false);
+        p.getInventory().addItem(sword);
+    }
+
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e){
+        LoginType loginType = checkLoginType(e.getPlayer());
+        if(loginType==LoginType.getbonuslogin){
+            e.setJoinMessage(loginMessage.replace("<player>",e.getPlayer().getName()));
+            updatePlayerLog(e.getPlayer(),true);
+            giveLoginBonus(e.getPlayer());
+        }else if(loginType==LoginType.firstlogin){
+            e.setJoinMessage(firstloginMessage.replace("<player>",e.getPlayer().getName()));
+            updatePlayerLog(e.getPlayer(),true);
+            firstLoginBonus(e.getPlayer());
+        }else{
+            e.setJoinMessage(loginMessage.replace("<player>",e.getPlayer().getName()));
+            updatePlayerLog(e.getPlayer(),false);
+        }
+    }
+
+}
